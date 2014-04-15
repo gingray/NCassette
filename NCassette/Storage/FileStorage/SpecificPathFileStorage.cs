16d26d7ac549f184110562bd11b276fc937ef14d
gingray <gingray.dev@gmail.com>
2014-04-15 23:33:24 +0400

    Add apportunity to save serialized data to custom folder also to duplicate data with custom key in storage

---
+++ NCassette/Storage/FileStorage/SpecificPathFileStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace NCassetteLib.Storage.FileStorage
+{
+    public class SpecificPathFileStorage:IStorage
+    {
+        private readonly string _folder;
+
+        public SpecificPathFileStorage(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            _folder = folder;
+        }
+        public byte[] GetFromStorage(string id)
+        {
+            var filename = Path.Combine(_folder, id);
+            if (!File.Exists(filename))
+            {
+                return null;
+            }
+            return File.ReadAllBytes(filename);
+        }
+
+        public void PutIntoStorage(byte[] data, string id)
+        {
+            var filename = Path.Combine(_folder, id);
+            File.WriteAllBytes(filename, data);
+        }
+    }
+}
